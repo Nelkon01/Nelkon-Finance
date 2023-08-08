e191afb22baf4c6952d25ecc702666b212406ab6
Oluwadamilola Oyenekan <adeniyioyenekan@yahoo.co.uk>
2023-08-08 21:35:34 +0100

    Add home.html to be home page

--- budgetmanager/routes.py
+++ budgetmanager/routes.py
@@ -1,8 +1,20 @@
 from flask import render_template, request, redirect, url_for
 from budgetmanager import app, db
-from budgetmanager.models import Users, Month, BudgetedIncome, ActualIncome, BudgetedExpenses, ActualExpenses
+from budgetmanager.models import Users, Month, BudgetedIncome, ActualIncome, BudgetedExpenses, ActualExpenses, Category
 
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/categories', methods=['GET'])
+def categories():
+    categories = ['Shopping', 'Groceries', 'Eating Out', 'Transport', 'Bills', 'Entertainment', 'Miscellaneous']
+
+    for category_name in categories:
+        category = Category(name=category_name)
+        db.session.add(category)
+
+    db.session.commit()
+    return "Categories added to database"
