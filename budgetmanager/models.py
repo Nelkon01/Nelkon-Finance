1b48d63d719102f05087b7e03642d9ad4588f17d
Oluwadamilola Oyenekan <adeniyioyenekan@yahoo.co.uk>
2023-08-11 02:32:38 +0100

    Update the db models to include relationships between schemas

--- budgetmanager/models.py
+++ budgetmanager/models.py
@@ -9,86 +9,68 @@ class Users(db.Model):
     lastname = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
+    budgeted_incomes = db.relationship("BudgetedIncome", backref="user", cascade="all, delete", lazy=True)
+    actual_incomes = db.relationship("ActualIncome", backref="user", cascade="all, delete", lazy=True)
+    budgeted_expenses = db.relationship("BudgetedIncome", backref="user", cascade="all, delete", lazy=True)
+    actual_expenses = db.relationship("ActualIncome", backref="user", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         # __repr__ method for Users class to return a string representation of the object
         return f"#:{self.id} -  Username: {self.username} - Firstname: {self.firstname} - Lastname: {self.lastname} - Password: {self.password} - Email: {self.email}"
 
 
-class Month(db.Model):
-    # schema for month table
-    month_id = db.Column(db.Integer, primary_key=True)
-    month_name = db.Column(db.String(20), nullable=False, unique=True)
-
-    def __repr__(self):
-        # __repr__ method for Month class to return a string representation of the object
-        return f"Month('{self.month_name}')"
-
-
-MONTH_ID = 'month.month_id'
 USER_ID = 'users.id'
 
 
 class BudgetedIncome(db.Model):
     # schema for budgeted income table
     income_id = db.Column(db.Integer, primary_key=True)
-    income_name = db.Column(db.String(20), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey(USER_ID), nullable=False)
-    month_id = db.Column(db.Integer, db.ForeignKey(MONTH_ID), nullable=False)
+    income_name = db.Column(db.String(50), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(USER_ID, ondelete="cascade"), nullable=False)
+    month_name = db.Column(db.String(20), nullable=False)
     budget_amount = db.Column(db.Float, nullable=False)
 
     def __repr__(self):
         # __repr__ method for BudgetedIncome class to return a string representation of the object
-        return f"BudgetedIncome('{self.income_name}', '{self.user_id}', '{self.month_id}', '{self.budget_amount}')"
+        return f"BudgetedIncome('{self.income_name}', '{self.user_id}', '{self.month_name}', '{self.budget_amount}')"
 
 
 class ActualIncome(db.Model):
     # schema for actual income table
     income_id = db.Column(db.Integer, primary_key=True)
     income_name = db.Column(db.String(20), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey(USER_ID), nullable=False)
-    month_id = db.Column(db.Integer, db.ForeignKey(MONTH_ID), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(USER_ID, ondelete="cascade"), nullable=False)
+    date = db.Column(db.Date, nullable=False)
     actual_amount = db.Column(db.Float, nullable=False)
 
     def __repr__(self):
         # __repr__ method for ActualIncome class to return a string representation of the object
-        return f"ActualIncome('{self.income_name}', '{self.user_id}', '{self.month_id}', '{self.actual_amount}')"
+        return f"ActualIncome('{self.income_name}', '{self.user_id}', '{self.date}' '{self.actual_amount}')"
 
 
 class BudgetedExpenses(db.Model):
     # schema for budgeted expenses table
     expense_id = db.Column(db.Integer, primary_key=True)
-    category_name = db.Column(db.String(20), db.ForeignKey(
-        'category.category_name'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey(USER_ID), nullable=False)
-    month_id = db.Column(db.Integer, db.ForeignKey(MONTH_ID), nullable=False)
+    category_name = db.Column(db.String(20), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(USER_ID, ondelete="cascade"), nullable=False)
+    month_name = db.Column(db.String(20), nullable=False)
     budget_amount = db.Column(db.Float, nullable=False)
 
     def __repr__(self):
         # __repr__ method for BudgetedExpenses class to return a string representation of the object
-        return f"BudgetedExpenses('{self.category_name}', '{self.user_id}', '{self.month_id}', '{self.budget_amount}')"
+        return f"BudgetedExpenses('{self.category_name}', '{self.user_id}', '{self.month_name}, '{self.budget_amount}')"
 
 
 class ActualExpenses(db.Model):
     # schema for actual expenses table
     expense_id = db.Column(db.Integer, primary_key=True)
     expense_name = db.Column(db.String(20), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey(USER_ID), nullable=False)
-    month_id = db.Column(db.Integer, db.ForeignKey(MONTH_ID), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(USER_ID, ondelete="cascade"), nullable=False)
+    date = db.Column(db.Date, nullable=False)
     category_name = db.Column(db.String(20), nullable=False)
     actual_amount = db.Column(db.Float, nullable=False)
 
     def __repr__(self):
         # __repr__ method for ActualExpenses class to return a string representation of the object
-        return (f"ActualExpenses('{self.expense_name}', '{self.user_id}', '{self.month_id}', '{self.category_name}',"
+        return (f"ActualExpenses('{self.expense_name}', '{self.user_id}', '{self.month_name}', '{self.category_name}',"
                 f" '{self.actual_amount}')")
-
-
-class Category(db.Model):
-    # schema for category table
-    id = db.Column(db.Integer, primary_key=True)
-    category_name = db.Column(db.String(20), nullable=False, unique=True)
-
-    def __repr__(self):
-        # __repr__ method for Category class to return a string representation of the object
-        return f"Category('{self.category_name}')"
